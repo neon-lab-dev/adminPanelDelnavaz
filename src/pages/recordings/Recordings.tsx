ec09e9b89854f3093941302e5604e253dcf9f10e
Rahul Sutradhar <rahulsd380@gmail.com>
2024-11-29 16:08:00 +0600

    Fixed deployment issues

--- src/pages/recordings/Recordings.tsx
+++ src/pages/recordings/Recordings.tsx
@@ -1,11 +1,13 @@
 import RecordingsCards from "./RecordingsCards";
 import RecordingTable from "./RecordingTable";
 
-export default function Recordings() {
+const Recordings = () => {
   return (
     <div className="p-5 flex flex-col gap-8">
       <RecordingsCards />
       <RecordingTable />
     </div>
   );
-}
+};
+
+export default Recordings;
